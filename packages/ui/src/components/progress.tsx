f167ee249799c20d13106ef0e00802f202f1169d
Rafa Al Razzak <rafaar@kita.blue>
2026-05-22 13:38:37 +0700

    fix: correct indentation in ProgressProps interface definition

--- packages/ui/src/components/progress.tsx
+++ packages/ui/src/components/progress.tsx
@@ -26,7 +26,7 @@ const progressVariants = cva("relative w-full overflow-hidden rounded-full", {
 
 export interface ProgressProps
   extends Omit<React.ComponentPropsWithoutRef<typeof ProgressBar>, "value">,
-    VariantProps<typeof progressVariants> {
+  VariantProps<typeof progressVariants> {
   value?: number;
 }
 
